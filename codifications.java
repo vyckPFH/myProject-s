b97152829685dc8447256a700ed40a98fc114a44
vyckPFH <victoria.c.oliveira08@gmail.com>
2025-03-10 16:08:33 -0300

    10/3/2025 - desafiando devs code/ feito em english/ desafio de codificação simples

--- codifications.java
+++ codifications.java
@@ -1,39 +1,38 @@
 import java.util.Scanner;
 
-public class codifications{
+public class codifications {
     final static Scanner LER = new Scanner(System.in);
-    
+
     public static void main(String[] args) {
-      //  String palabra = LER.next();
-        imprintCode();
+         String palabra = LER.next();
+        imprintCode(palabra);
     }
 
-    public static void imprintCode(){
-       int [] array;
-       array =  numCodification("macaco");      
+    public static void imprintCode(String palabra) {
+        int[] array;
+        array = numCodification(palabra);
         for (int i = 0; i < array.length; i++) {
             System.out.println(array[i] + " ");
         }
     }
 
-
-    public static int[] numCodification(String word){
-        char[] wordInChar;
-        char[] alfabeto = {' ' , 'a' , 'b' , 'c', 'd' , 'e' , 'f' , 'g' , 'j' , 'h' , 'i' , 'j' , 'k' , 'l' , 'm' , 'n' , 'o' , 'p' , 'q' , 'r' , 's' , 't' , 'u' , 'v' , 'w' , 'x' , 'y' , 'z' ,};
+    public static int[] numCodification(String word) {
+        char[] wordInChar = word.toCharArray();
+        char[] alfabeto = { ' ', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'j', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p',
+                'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', };
         int arrayLength = word.length();
-        int[] wordCodificated = new int[arrayLength + 1];
-        int j = 0;
-       
+        int[] wordCodificated = new int[arrayLength];
+
         for (int i = 0; i < wordCodificated.length; i++) {
-            
 
-            char[] charcAtI = word.toCharArray(i);
+            for (int k = 0; k < alfabeto.length; k++) {
 
-            if (charcAtI == alfabeto[i]) {
-                wordCodificated[j] = i;
-                j++;
+                if (wordInChar[i] == alfabeto[k]) {
+                    wordCodificated[i] = k;
+                }
+
             }
-            
+
         }
 
         return wordCodificated;
